85ce558ab0ec658b2e1bb6b07e5dadc3c1400ae7
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-09-02 13:40:28 -0600

    add test and code to make test pass

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,36 @@
-function nameMenuItem() {
-
+function nameMenuItem( item ) {
+  return `Delicious ${item}`
 }
-
-
+function createMenuItem( item, price, type){
+  return {
+    name: item,
+    price,
+    type
+  }
+}
+function addIngredients( ingredient, array){
+  if(!array.includes(ingredient)){
+    array.push(ingredient)
+  }
+}
+function formatPrice( price ){
+  return `$${price}`
+}
+function decreasePrice( price ){
+  return (price - (price * .10))
+}
+function createRecipe( title, ingredients, type ){
+  return {
+    title,
+    ingredients,
+    type: type
+  }
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
